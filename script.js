0cca6bebbb34d6c1b21049dba2b9b7a76caa5b6a
lex23unwin <lex23unwin@gmail.com>
2023-08-13 11:41:52 -0500

    gameBoard module IIFE set up

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ const PlayerFactory = (sign) => {
 
     return { getSign };
 }
+
+const gameBoard = (() => {
+
+    let board = ["", "", "", "", "", "", "", "", ""];
+
+    const setSquare = (index, symbol) => {
+        board[index] = symbol;
+    };
+
+    const getSquare = (index) => {
+        return board[index];
+    };
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++)
+        {
+            board[i] = "";
+        }
+    };
+
+    return { setSquare, getSquare, reset };
+})(); 
