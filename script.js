9e0808af554ea10a6183475c1a4e3c5190995e39
lex23unwin <lex23unwin@gmail.com>
2023-08-13 11:43:15 -0500

    gameController but how do i checkwinner?!?!?!?!

--- script.js
+++ script.js
@@ -28,3 +28,67 @@ const gameBoard = (() => {
 
     return { setSquare, getSquare, reset };
 })(); 
+
+const gameController = (() => {
+
+    const player1 = PlayerFactory("X");
+    const player2 = PlayerFactory("O");
+
+    let round = 0;
+    let isOver = false;
+
+    const getRound = () => {
+        return round; 
+    };
+
+    const getIsOver = () => {
+        return isOver;
+    };
+
+    const reset = () => {
+        round = 1;
+        isOver = false;
+    };
+
+    const getCurrentPlayerSign = () => {
+        if (round % 2 == 0)
+        {
+            return player2.getSign(); 
+        }
+        else 
+        {
+            return player1.getSign();
+        }
+    };
+
+    const getCurrentPlayerName = () => {
+        if (round % 2 == 0)
+        {
+            return "Player 2";
+        }
+        else 
+        {
+            return "Player 1";
+        }
+    };  
+
+    const playRound = (squareIndex) => {
+        gameBoard.setSquare(squareIndex, getCurrentPlayerSign());
+        if (checkWinner())
+        {
+            isOver = true;
+            round += 1;
+            return;
+        }
+        if (round === 9)
+        {
+            isOver = true;
+
+            return;
+        }
+        round += 1;
+    };
+
+    return { getRound, getIsOver, reset, playRound, getCurrentPlayerSign, getCurrentPlayerName };
+
+})();
